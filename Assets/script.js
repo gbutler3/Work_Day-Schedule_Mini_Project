a2b6004965242e64ebc20d7e9f96290c032a3b94
grace butler <gmx.butler@gmail.com>
2021-06-06 14:44:38 -0500

    trying to figure out how to apply the colors, but it just show up green

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-//-----CURRENT MONTH, DAY YEAR, AND TIME AT TOP OF PAGE
+//-----CURRENT MONTH, DAY YEAR, AND TIME AT TOP OF PAGE-------------
 
     //this shows the current weekday, month, day, and year. 
     function today(){
@@ -8,8 +8,8 @@
     setInterval(today, 1000)
 
 $(document).ready(function () { //Once the html loads all of these functions will be ready to work 
-   
-//-----SAVE BUTTON----------------------------------
+
+//-----SAVE BUTTON--------------------------------------------------
     
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
@@ -22,7 +22,7 @@ $(document).ready(function () { //Once the html loads all of these functions wil
         localStorage.setItem(time, description);
     });
 
-//-----CLEAR BUTTON----------------------------------
+//-----CLEAR BUTTON-------------------------------------------------
 
     //event listener for clear button
     $(".clearBtn").on("click", function (){
@@ -36,19 +36,45 @@ $(document).ready(function () { //Once the html loads all of these functions wil
         $(".description").val("");
     })
 
-
-//-----LOCAL STORAGE CALL BACK ON RELOAD-------------
+//-----LOCAL STORAGE CALL BACK ON RELOAD----------------------------
 
     //This pulls anything that is saved in local storage to display on the page if reloaded because if not it just gets displayed in the localStorage area and not on the actual webpage. 
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
     $("#hour1 .description").val(localStorage.getItem("hour1"));
     $("#hour2 .description").val(localStorage.getItem("hour2"));
     $("#hour3 .description").val(localStorage.getItem("hour3"));
     $("#hour4 .description").val(localStorage.getItem("hour4"));
     $("#hour5 .description").val(localStorage.getItem("hour5"));
     $("#hour6 .description").val(localStorage.getItem("hour6"));
-    $("#hour7 .description").val(localStorage.getItem("hour7"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
+
+//-----COLOR CHANGES PER TIME-BLOCK PER CURRENT TIME----------------
+
+//TODO: figure out why these aren't showing the colors correctly. 
+function colorChanges (){
+
+    var currentHour = moment().hour();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var timeSlot = parseInt($(this).attr("id").split("hour")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (timeSlot > currentHour) { //if the time slot is greater than the current hour mark it as past 
+            $(this).addClass("past");
+        }
+        else if (timeSlot < currentHour) { //if the time slot is less than the current hour mark it as present
+            $(this).addClass("future");
+        }
+        else {
+            $(this).addClass("present"); 
+
+        }
+    })
+        
+}
+colorChanges();
 });
