fe266fcd89756bbd1471c5f0256ed7f40d51dd70
grace butler <gmx.butler@gmail.com>
2021-06-05 22:35:28 -0500

    added comments and sectioned js for clarity

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,34 @@
-console.log("test")
+//-----CURRENT MONTH, DAY YEAR, AND TIME AT TOP OF PAGE
 
-//this shows the current weekday, month, day, and year. 
-function today(){
+    //this shows the current weekday, month, day, and year. 
+    function today(){
     var today = moment().format('dddd, MMM Do YYYY, h:mm a');
     $("#currentDay").text(today);
     }
     setInterval(today, 1000)
 
-$(document).ready(function () { //once the html loads all of these functions will be ready to work 
+$(document).ready(function () { //Once the html loads all of these functions will be ready to work 
+   
+//-----SAVE BUTTON----------------------------------
+    
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
         // Get nearby values of the description in JQuery
         var description = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        console.log("save button is being called")
+        console.log("Save button is being called")
 
         // Save text in local storage
         localStorage.setItem(time, description);
     });
 
-    //Clears on the local storage area
+//-----CLEAR BUTTON----------------------------------
+
+    //event listener for clear button
     $(".clearBtn").on("click", function (){
+        //Clears on the local storage area
         window.localStorage.clear();
-        console.log("clear localstorage");
+        //console.log("clear localstorage");
     });
 
     //clears the text out of the textarea
@@ -30,8 +36,10 @@ $(document).ready(function () { //once the html loads all of these functions wil
         $(".description").val("");
     })
 
-    //This pulls anything that is saved in local storage to display on the page if reloaded because if not it just gets displayed in the 
-    //localStorage area and not on the actual webpage. 
+
+//-----LOCAL STORAGE CALL BACK ON RELOAD-------------
+
+    //This pulls anything that is saved in local storage to display on the page if reloaded because if not it just gets displayed in the localStorage area and not on the actual webpage. 
     $("#hour1 .description").val(localStorage.getItem("hour1"));
     $("#hour2 .description").val(localStorage.getItem("hour2"));
     $("#hour3 .description").val(localStorage.getItem("hour3"));
