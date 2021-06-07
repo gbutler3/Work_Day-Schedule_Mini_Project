e7f266f5e494c43fee3d263b8ba386d0bd2ae1a4
grace butler <gmx.butler@gmail.com>
2021-06-07 09:46:22 -0500

    bullet points for readme file. had to change the hours to 24 hour time in the back end for the comparison for the color blocks. used console.log to figure out what was being compared to change and fix in the html and js.

--- Assets/script.js
+++ Assets/script.js
@@ -16,7 +16,7 @@ $(document).ready(function () { //Once the html loads all of these functions wil
         // Get nearby values of the description in JQuery
         var description = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        console.log("Save button is being called")
+        // console.log("Save button is being called")
 
         // Save text in local storage
         localStorage.setItem(time, description);
@@ -39,42 +39,44 @@ $(document).ready(function () { //Once the html loads all of these functions wil
 //-----LOCAL STORAGE CALL BACK ON RELOAD----------------------------
 
     //This pulls anything that is saved in local storage to display on the page if reloaded because if not it just gets displayed in the localStorage area and not on the actual webpage. 
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16.description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    $("#hour6 .description").val(localStorage.getItem("hour6"));
 
 //-----COLOR CHANGES PER TIME-BLOCK PER CURRENT TIME----------------
 
-//TODO: figure out why these aren't showing the colors correctly. 
 function colorChanges (){
 
-    var currentHour = moment().hour();
+    var currentHour = moment().format("HH"); //make this in 24 hours
+    //console.log(currentHour)
 
     // loop over time blocks
     $(".time-block").each(function () {
         var timeSlot = parseInt($(this).attr("id").split("hour")[1]);
+        //console.log(timeSlot) 
+
 
         // To check the time and add the classes for background indicators
-        if (timeSlot > currentHour) { //if the time slot is greater than the current hour mark it as past 
+        if (timeSlot < currentHour) { //if the time slot is greater than the current hour mark it as past 
             $(this).addClass("past");
+            //console.log("past")
         }
-        else if (timeSlot < currentHour) { //if the time slot is less than the current hour mark it as present
+        else if (timeSlot > currentHour) { //if the time slot is less than the current hour mark it as future
             $(this).addClass("future");
+            //console.log("future")
         }
         else {
             $(this).addClass("present"); 
-
         }
-    })
-        
+    }) 
 }
 colorChanges();
 });
--- README.md
+++ README.md
@@ -8,19 +8,12 @@ You'll need to use the [Moment.js](https://momentjs.com/) library to work with d
 
 ## Goals
 
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+- WHEN I open the planner the current day is displayed at the top of the calendar
+- WHEN I scroll down, I am presented with timeblocks for standard business hours
+- WHEN I view the timeblocks for that day, each timeblock is color coded to indicate whether it is in the past, present, or future
+- WHEN I click into a timeblock, I can enter an event
+- WHEN I click the save button for that timeblock, the text for that event is saved in local storage
+- WHEN I refresh the page, the saved events persist
 
 ## Screen Shot
 
