3d1efc08cf635496477fbf0a7c39fa1fa52883bf
grace butler <gmx.butler@gmail.com>
2021-06-07 10:08:24 -0500

    double checked that all local storage was working for each time slot. had to fix a few

--- Assets/script.js
+++ Assets/script.js
@@ -42,14 +42,14 @@ $(document).ready(function () { //Once the html loads all of these functions wil
     $("#hour13 .description").val(localStorage.getItem("hour13"));
     $("#hour14 .description").val(localStorage.getItem("hour14"));
     $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16.description").val(localStorage.getItem("hour16"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
     $("#hour18 .description").val(localStorage.getItem("hour18"));
-    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
 
 //-----COLOR CHANGES PER TIME-BLOCK PER CURRENT TIME----------------
 
